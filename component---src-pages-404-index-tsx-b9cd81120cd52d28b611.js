0f150232a4ad247ed594a11471d986bd458a6966
visiky <736929286@qq.com>
2021-02-24 13:05:33 +0800

    Updates

---
+++ component---src-pages-404-index-tsx-b9cd81120cd52d28b611.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{GEoZ:function(e,t,n){"use strict";n.r(t);var a=n("yDyU"),u=n.n(a),o=n("N5vR");t.default=function(){return u.a.createElement(o.a,null,u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-index-tsx-b9cd81120cd52d28b611.js.map
