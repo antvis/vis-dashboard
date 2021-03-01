c6d6a2d09cf02dc5568528f21ca622c98181c98b
沈建斌 <wb-sjb709429@antfin.com>
2021-03-01 15:29:36 +0800

    feat: 添加主题色切换switcher

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch } from 'antd';
 
 type Props = {
   siteTitle: string;
@@ -8,6 +9,7 @@ const Header: React.FC<Props> = ({ siteTitle }) => (
   <div className="site-header">
     <div className="site-title">
       <h1 style={{ margin: 0 }}>{siteTitle}</h1>
+      <Switch className="theme-switcher" unCheckedChildren='日间' checkedChildren='夜间'/>
     </div>
   </div>
 );
