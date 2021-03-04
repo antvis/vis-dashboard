bdfd3fc9b20ffb918f361f599fed8f2134660d1c
沈建斌 <wb-sjb709429@antfin.com>
2021-03-04 14:49:47 +0800

    feat: 完善common/header切换样式按钮

--- src/components/x-plot/common/header.tsx
+++ src/components/x-plot/common/header.tsx
@@ -7,20 +7,30 @@ import { DownOutlined } from '@ant-design/icons';
 
 export const Header: React.FC<XComponentProps> = ({ attributes }) => {
   const { title, options } = attributes;
-  console.log('options', options)
+
+  const onMenuClick = (e) => {
+    const key = e.key;
+    // to do something....
+  }
+
   const overlay = _.isEmpty(options) ? null : (
-    <Menu>
+    <Menu onClick={onMenuClick}>
       {
-        _.map(options, option => (
-          <Menu.Item key={option.value}>{option.label}</Menu.Item>
+        _.map(options, (option, idx) => (
+          <Menu.Item key={idx}>{option.label}</Menu.Item>
         ))
       }
     </Menu>
   )
+
   return (
     <div className={cx('header', 'grid-item-move-handler', attributes.moveHandlerClassName)}>
       <span className="title">{title}</span>
-      {!_.isEmpty(options) && <Dropdown overlay={overlay}><span className={'style-switcher'}>切换样式<DownOutlined /></span></Dropdown>}
+      {_.size(options) > 1 && (
+        <Dropdown overlay={overlay}>
+          <span className={'style-switcher'}>切换样式<DownOutlined /></span>
+        </Dropdown>
+      )}
     </div>
   );
 };
