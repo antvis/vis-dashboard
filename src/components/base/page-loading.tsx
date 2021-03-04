66c8efe0a5b8780ec3811d92d8d9eb0371257025
沈建斌 <wb-sjb709429@antfin.com>
2021-03-04 14:48:49 +0800

    feat: pageLoading 组建增强

--- src/components/base/page-loading.tsx
+++ src/components/base/page-loading.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CodeLoading from './code-loading';
 
-const PageLoading: React.FC = () => (
-  <div style={{ height: '100vh' }}>
-    <CodeLoading />
-  </div>
-);
+const PageLoading: React.FC = () => {
+
+  useEffect(() => {
+    // 为了确保遮罩层下，没有滚动事件
+    const oldOverflow = document.body.style.overflow;
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = oldOverflow;
+    }
+  }, [])
+
+  return (
+    <div style={{ height: '100vh' }}>
+      <CodeLoading />
+    </div>
+  )
+};
 
 export default PageLoading;
