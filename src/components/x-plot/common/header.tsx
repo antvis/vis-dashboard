a5ebc40d4130ecdf35fdfa039c948dabb7fa66f9
沈建斌 <wb-sjb709429@antfin.com>
2021-03-04 16:15:48 +0800

    feat: 完善头部样式切换按钮

--- src/components/x-plot/common/header.tsx
+++ src/components/x-plot/common/header.tsx
@@ -5,19 +5,24 @@ import { Dropdown, Menu } from 'antd';
 import * as _ from 'lodash';
 import { DownOutlined } from '@ant-design/icons';
 
-export const Header: React.FC<XComponentProps> = ({ attributes }) => {
+type HeaderProps = {
+  changeOption?: (number) => void;
+  selectedOption?: number;
+} & XComponentProps;
+
+export const Header: React.FC<HeaderProps> = ({ attributes, changeOption, selectedOption = 0 }) => {
   const { title, options } = attributes;
 
   const onMenuClick = (e) => {
     const key = e.key;
-    // to do something....
+    changeOption && changeOption(key);
   }
 
   const overlay = _.isEmpty(options) ? null : (
-    <Menu onClick={onMenuClick}>
+    <Menu onClick={onMenuClick} selectedKeys={[String(selectedOption)]} className='header-style-switcher-menu'>
       {
         _.map(options, (option, idx) => (
-          <Menu.Item key={idx}>{option.label}</Menu.Item>
+          <Menu.Item key={idx} className='menu-item'>{`样式${idx+1}`}</Menu.Item>
         ))
       }
     </Menu>
@@ -27,7 +32,7 @@ export const Header: React.FC<XComponentProps> = ({ attributes }) => {
     <div className={cx('header', 'grid-item-move-handler', attributes.moveHandlerClassName)}>
       <span className="title">{title}</span>
       {_.size(options) > 1 && (
-        <Dropdown overlay={overlay}>
+        <Dropdown overlay={overlay} >
           <span className={'style-switcher'}>切换样式<DownOutlined /></span>
         </Dropdown>
       )}
