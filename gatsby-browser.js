59c671ed75606a0eb16497370050489b3ea8930d
visiky <736929286@qq.com>
2021-05-05 01:24:54 +0800

    fix: 修复 antd 样式问题

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,5 @@ window.g2 = require('@antv/g2');
 window.dataSet = require('@antv/data-set');
 window.antd = require('antd');
 window.lodash = require('lodash');
+
+require('antd/lib/message/style/index.css');
